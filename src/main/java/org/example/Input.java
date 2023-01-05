1ad8db867aa9615a152f2158732a9d095ffb9b75
LemonRiz <95.m.rizvi@gmail.com>
2023-01-05 15:40:20 +0000

    added first dictionary words

---
+++ src/main/java/org/example/Input.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+package org.example;
+
+
+public class Input {
+    private static Scanner scanner = new Scanner(System.in);
+
+}
---
+++ src/main/java/org/example/Storage.java
@@ -0,0 +1,18 @@
+package org.example;
+import java.util.ArrayList;
+
+public class Storage {
+    private static ArrayList<String> words = new ArrayList<>();
+
+    static {
+        words.add("attraction");
+        words.add("biology");
+        words.add("chemical");
+        words.add("diving");
+        words.add("elephant");
+        words.add("farming");
+        words.add("gravity");
+        words.add("hologram");
+        words.add("individual");
+    }
+}
